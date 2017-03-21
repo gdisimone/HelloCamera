3e4f27e0f06957cc13219188c6d67d57655f5c04
gdisimone <gios_eppo@hotmail.it>
2017-03-21 13:40:33 +0100

    Clean up

--- HelloCamera/Program.cs
+++ HelloCamera/Program.cs
@@ -23,9 +23,6 @@ namespace HelloCamera
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
-        //    byte[] bytes2 = new byte[320 * 240 * 3 + 54];
-        //    GT.Picture picture = new GT.Picture(bytes2, GT.Picture.PictureEncoding.BMP);
-
             // Initialize event handlers here.
             button.ButtonPressed += new Button.ButtonEventHandler(button_ButtonPressed);
             camera.PictureCaptured += new Camera.PictureCapturedEventHandler(camera_PictureCaptured);
@@ -36,21 +33,10 @@ namespace HelloCamera
             
         void camera_PictureCaptured(Camera sender, GT.Picture picture)
         {
-            //Bitmap photo1 = new Bitmap(picture.PictureData, Bitmap.BitmapImageType.Bmp);
-            
-            //string pathfilename = "C:\\Users\\gios_\\Documents\\Visual Studio 2013\\Projects\\HelloCamera\\image1.bmp";
             string pathfilename = "\\SD\\BARCODE\\image" + index.ToString() + ".bmp";
             index++;
-            //display.SimpleGraphics.DisplayImage(picture, 5, 5);
-
-            //File.Create(pathfilename);
-            //File.WriteAllBytes(pathfilename, picture.PictureData);
 
             sdCard.StorageDevice.WriteFile(pathfilename, picture.PictureData);
-
-            //photo1.Save(pathfilename);
-
-            //System.IO.File.WriteAllBytes(@"C:\image.bmp", picture.PictureData);
 
             Debug.Print("Picture Saved.");
         }
